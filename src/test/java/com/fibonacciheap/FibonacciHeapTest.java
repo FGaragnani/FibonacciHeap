394ee922389ceb059ddbe6d0642fdce7a08949b1
FGaragnani <filippogaragnani1@gmail.com>
2023-06-10 19:07:01 +0200

    Added tests for size() and isEmpty()

--- src/test/java/com/fibonacciheap/FibonacciHeapTest.java
+++ src/test/java/com/fibonacciheap/FibonacciHeapTest.java
@@ -8,10 +8,18 @@ class FibonacciHeapTest {
 
     @Test
     void size() {
+        FibonacciHeap<Integer> fibonacciHeap = new FibonacciHeap<>(3, 4, 1, 5, 4, 3);
+        assertEquals(fibonacciHeap.size(), 6);
+        fibonacciHeap.add(8);
+        assertEquals(fibonacciHeap.size(), 7);
     }
 
     @Test
     void isEmpty() {
+        FibonacciHeap<Integer> fibonacciHeap = new FibonacciHeap<>();
+        assertTrue(fibonacciHeap.isEmpty());
+        fibonacciHeap.add(3);
+        assertFalse(fibonacciHeap.isEmpty());
     }
 
     @Test
